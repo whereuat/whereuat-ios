8da2173f57842d759c04f51e551bcf1e26c24f13
Raymond Jacobson <me@raymondjacobson.com>
2016-02-18 16:28:10 -0500

    Add contact cards with animation on longpress

---
+++ ContactView.swift
@@ -0,0 +1,78 @@
+//
+//  ContactView.swift
+//  whereuat-ios
+//
+//  Created by Raymond Jacobson on 2/11/16.
+//  Copyright © 2016 whereu@. All rights reserved.
+//
+
+import UIKit
+
+class ContactView: UIView {
+    
+    var displayContactView: UIView!
+    var editContactView: UIView!
+    var displayContactMode = true
+    
+    var contactColor: UIColor!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.initialize()
+    }
+    
+    func initialize() {
+        // Initialize views
+        displayContactView = UIView()
+        editContactView = UIView()
+        contactColor = self.randomColor()
+        
+        displayContactView.backgroundColor = contactColor
+        editContactView.backgroundColor = UIColor(red: 248/255, green: 247/255, blue: 243/255, alpha: 1.0)
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: Selector("contactCardFlip:"))
+        longPress.minimumPressDuration = 0.25
+        self.addGestureRecognizer(longPress)
+        self.userInteractionEnabled = true
+
+        self.addConstrainedSubview(displayContactView)
+        
+    }
+    
+    func contactCardFlip(sender: UILongPressGestureRecognizer) {
+        if (sender.state != UIGestureRecognizerState.Ended) {
+            return
+        }
+        if (displayContactMode) {
+            UIView.transitionFromView(displayContactView, toView: editContactView, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil);
+            displayContactMode = false
+            self.addConstrainedSubview(editContactView)
+        } else {
+            self.addSubview(displayContactView)
+            UIView.transitionFromView(editContactView, toView: displayContactView, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+            displayContactMode = true
+            self.addConstrainedSubview(displayContactView)
+        }
+    }
+    
+    func addConstrainedSubview(viewName: UIView) {
+        // Constrain the size of a view so that it conforms to self's size (constrained to screen-size adjusted ContactCell)
+        self.addSubview(viewName)
+        
+        viewName.translatesAutoresizingMaskIntoConstraints = false
+        let leftSideConstraint = NSLayoutConstraint(item: viewName, attribute: .Left, relatedBy: .Equal, toItem: self, attribute: .Left, multiplier: 1.0, constant: 0.0)
+        let bottomConstraint = NSLayoutConstraint(item: viewName, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        let widthConstraint = NSLayoutConstraint(item: viewName, attribute: .Width, relatedBy: .Equal, toItem: self, attribute: .Width, multiplier: 1.0, constant: 0.0)
+        let heightConstraint = NSLayoutConstraint(item: viewName, attribute: .Height, relatedBy: .Equal, toItem: self, attribute: .Height, multiplier: 1.0, constant: 0.0)
+        self.addConstraints([leftSideConstraint, bottomConstraint, widthConstraint, heightConstraint])
+    }
+    
+    func randomColor() -> UIColor{
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen:CGFloat = CGFloat(drand48())
+        let randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+
+}
--- whereuat-ios/AppDelegate.swift
+++ whereuat-ios/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+let themeColor = UIColor(red: 0.01, green: 0.41, blue: 0.22, alpha: 1.0)
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window?.tintColor = themeColor
         return true
     }
 
