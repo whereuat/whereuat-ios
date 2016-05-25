f799817584c373b24777a86b19960d7936a1b3d5
Raymond Jacobson <raymondshujacobson8@gmail.com>
2016-05-24 20:10:33 -0400

    IOS #46 Implement Settings page (#56)

--- whereuat-ios/Views/SettingsView/SettingsViewController.swift
+++ whereuat-ios/Views/SettingsView/SettingsViewController.swift
@@ -12,11 +12,23 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.drawVersionTextView()
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.setNavigationBarItems("Settings")
+    }
+    
+    func drawVersionTextView() {
+        let textBoxWidth = CGFloat(100)
+        let versionTextView = UITextView(frame: CGRect(x: (self.view.frame.size.width/2 - textBoxWidth/2), y: 100, width: 100, height: 200))
+        let versionNumber: AnyObject? = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"]
+        versionTextView.text = "whereu@ Version " + String(versionNumber!)
+        versionTextView.font = FontStyle.p
+        versionTextView.textColor = ColorWheel.darkGray
+        versionTextView.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(versionTextView)
     }
     
 }
